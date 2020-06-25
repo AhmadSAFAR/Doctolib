5b87dea46d07acda917707f78f9e57bdd35e67e5
Frutios <benjamin.tourneur7@gmail.com>
2020-06-25 12:26:11 +0200

    tout refais

--- migrations/Version20200625102039.php
+++ migrations/Version20200625102039.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200625090906 extends AbstractMigration
+final class Version20200625102039 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,8 +20,8 @@ final class Version20200625090906 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE doctor (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, speciality VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, zip INT NOT NULL, adress LONGTEXT NOT NULL, phone_number INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE patient (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, zip VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, phone_number VARCHAR(255) NOT NULL, disease VARCHAR(255) NOT NULL, description VARCHAR(500) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE doctor (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, speciality VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, zip VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, phone_number VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE patient (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, phone_number VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, zip VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, description VARCHAR(500) NOT NULL, disease VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE patient_doctor (patient_id INT NOT NULL, doctor_id INT NOT NULL, INDEX IDX_148E1A906B899279 (patient_id), INDEX IDX_148E1A9087F4FB17 (doctor_id), PRIMARY KEY(patient_id, doctor_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE patient_doctor ADD CONSTRAINT FK_148E1A906B899279 FOREIGN KEY (patient_id) REFERENCES patient (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE patient_doctor ADD CONSTRAINT FK_148E1A9087F4FB17 FOREIGN KEY (doctor_id) REFERENCES doctor (id) ON DELETE CASCADE');
--- src/Entity/Patient.php
+++ src/Entity/Patient.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace App\Entity;
+
 use App\Repository\PatientRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+
 /**
  * @ORM\Entity(repositoryClass=PatientRepository::class)
  */
@@ -15,130 +18,192 @@ class Patient
      * @ORM\Column(type="integer")
      */
     private $id;
+
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $firstname;
+
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $lastname;
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $city;
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $zip;
-    /**
-     * @ORM\Column(type="string")
-     */
-    private $adress;
+
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $phoneNumber;
+
     /**
-     * @ORM\ManyToMany(targetEntity=Doctor::class, inversedBy="patients")
+     * @ORM\Column(type="string", length=255)
      */
-    private $doctor;
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $zip;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adress;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $description;
+
     /**
      * @ORM\Column(type="string", length=255)
      */
     private $disease;
+
     /**
-     * @ORM\Column(type="string", length=500 , nullable=true)
+     * @ORM\Column(type="string", length=255)
      */
-    private $description;
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Doctor::class, inversedBy="patients")
+     */
+    private $doctor;
+
     public function __construct()
     {
         $this->doctor = new ArrayCollection();
     }
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
+
     public function setFirstname(string $firstname): self
     {
         $this->firstname = $firstname;
+
         return $this;
     }
+
     public function getLastname(): ?string
     {
         return $this->lastname;
     }
+
     public function setLastname(string $lastname): self
     {
         $this->lastname = $lastname;
+
         return $this;
     }
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-        return $this;
-    }
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-        return $this;
-    }
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-    public function setCity(string $city): self
-    {
-        $this->city = $city;
-        return $this;
-    }
-    public function getZip(): ?string
-    {
-        return $this->zip;
-    }
-    public function setZip(string $zip): self
-    {
-        $this->zip = $zip;
-        return $this;
-    }
-    public function getAdress(): ?string
-    {
-        return $this->adress;
-    }
-    public function setAdress(string $adress): self
-    {
-        $this->adress = $adress;
-        return $this;
-    }
+
     public function getPhoneNumber(): ?string
     {
         return $this->phoneNumber;
     }
+
     public function setPhoneNumber(string $phoneNumber): self
     {
         $this->phoneNumber = $phoneNumber;
+
         return $this;
     }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getZip(): ?string
+    {
+        return $this->zip;
+    }
+
+    public function setZip(string $zip): self
+    {
+        $this->zip = $zip;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDisease(): ?string
+    {
+        return $this->disease;
+    }
+
+    public function setDisease(string $disease): self
+    {
+        $this->disease = $disease;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
     /**
      * @return Collection|Doctor[]
      */
@@ -146,36 +211,22 @@ class Patient
     {
         return $this->doctor;
     }
+
     public function addDoctor(Doctor $doctor): self
     {
         if (!$this->doctor->contains($doctor)) {
             $this->doctor[] = $doctor;
         }
+
         return $this;
     }
+
     public function removeDoctor(Doctor $doctor): self
     {
         if ($this->doctor->contains($doctor)) {
             $this->doctor->removeElement($doctor);
         }
-        return $this;
-    }
-    public function getDisease(): ?string
-    {
-        return $this->disease;
-    }
-    public function setDisease(string $disease): self
-    {
-        $this->disease = $disease;
-        return $this;
-    }
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-    public function setDescription(?string $description): self
-    {
-        $this->description = $description;
+
         return $this;
     }
 }
